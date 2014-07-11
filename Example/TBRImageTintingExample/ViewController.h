c86741d855b4f326d4406fd44b835f6b44dbbd10
Luciano Marisi <lucianomarisi@gmail.com>
2014-07-11 11:31:08 +0100

    Added example project

---
+++ Example/TBRImageTintingExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TBRImageTintingExample
+//
+//  Created by Luciano Marisi on 11/07/2014.
+//  Copyright (c) 2014 TechBrewers LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
